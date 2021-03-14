01e1a00333ca688ac8b959c8eaf73741b3b99935
ANDRII ALENIN <mkalenin@gmail.com>
2021-03-14 19:35:08 +0200

    Очищает значение атрибутов alt, src

--- js/cards.js
+++ js/cards.js
@@ -1,14 +1,21 @@
-import itemList from './gallery-items.js'
+import itemList from './gallery-items.js';
 const galRef = document.querySelector('.js-gallery');
-const closeModalBtnRef = document.querySelector('button[data-action="close-lightbox"]');
-const lightboxOverlayRef = document.querySelector('.js-lightbox > .lightbox__overlay');
+const closeModalBtnRef = document.querySelector(
+    'button[data-action="close-lightbox"]',
+);
+const lightboxOverlayRef = document.querySelector(
+    '.js-lightbox > .lightbox__overlay',
+);
 const bodyRef = document.querySelector('body');
 const lightboxRef = document.querySelector('.js-lightbox');
-const lightboxImageRef = document.querySelector('.lightbox__content > .lightbox__image');
+const lightboxImageRef = document.querySelector(
+    '.lightbox__content > .lightbox__image',
+);
 
 const createGalItem = items => {
-    return items.map(({ preview, original, description }) => {
-        return `<li class="gallery__item">
+    return items
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__item">
     <a
     class="gallery__link"
     href='${original}'
@@ -23,43 +30,47 @@ const createGalItem = items => {
   loading = "lazy"
   />
   </a>
-  </li>`
-    }).join(' ')
+  </li>`;
+        })
+        .join(' ');
 };
 
 const changeSettings = e => {
     lightboxImageRef.src = e.target.dataset.source;
-    lightboxImageRef.alt = e.target.alt
-}
+    lightboxImageRef.alt = e.target.alt;
+};
 
 const onOpenModalBtnCLick = e => {
     if (e.target.nodeName !== 'IMG') {
         return;
     }
+
     e.preventDefault();
     lightboxRef.classList.add('is-open');
-    changeSettings(e)
-
+    changeSettings(e);
 };
 
-const onCloseModalBtnClick = (e) => {
-    lightboxRef.classList.remove('is-open')
-    changeSettings(e)
+const onCloseModalBtnClick = e => {
+    lightboxRef.classList.remove('is-open');
+    lightboxImageRef.src = '';
+    lightboxImageRef.alt = '';
 };
 
 const onEscKeydown = e => {
     if (e.keyCode !== 27) {
-        return
+        return;
     }
-    lightboxRef.classList.remove('is-open')
-    changeSettings(e)
-}
+
+    onCloseModalBtnClick();
+};
 
 const createMarkup = createGalItem(itemList);
+
 galRef.insertAdjacentHTML('beforeend', createMarkup);
 
 bodyRef.addEventListener('keydown', onEscKeydown);
 
 galRef.addEventListener('click', onOpenModalBtnCLick);
+
 closeModalBtnRef.addEventListener('click', onCloseModalBtnClick);
 lightboxOverlayRef.addEventListener('click', onCloseModalBtnClick);
