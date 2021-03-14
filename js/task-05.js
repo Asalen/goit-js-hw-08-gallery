77b902ba758d46547de99d95a1e97234588ece2d
ANDRII ALENIN <mkalenin@gmail.com>
2021-03-14 10:34:22 +0200

    Добавляет базовые файлы

--- js/task-05.js
+++
@@ -1,9 +0,0 @@
-let inputText = document.querySelector('#name-input');
-inputText.addEventListener('input', inputChange);
-
-let outputText = document.querySelector('#name-output');
-
-function inputChange(event) {
-    inputText = event.currentTarget.value;
-    outputText.innerText = inputText ? inputText : "незнакомец";
-}
--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const inputSize = document.querySelector('#font-size-control');
-
-const spanText = document.querySelector('#text');
-
-const inputMove = e => {
-    spanText.style.fontSize = `${e.target.value}px`;
-}
-
-inputSize.addEventListener('input', inputMove);
